937b17bc4c7ffa57de55d5a06ed06ed5f1716e43
Miraceli <miraceli@live.com>
2025-08-14 18:46:57 -0300

    feat: configs material ui theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import theme from './theme.ts'
+
+import '@fontsource/inter/300.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/700.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
 )
+
+
